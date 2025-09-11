99502dc412fd7d06e759eed6dfb9c6b480bbc478
124049927-lang <124049927@upq.edu.mx>
2025-09-10 18:29:20 -0600

    explicación de conceptos básicos.

--- JsParaPrincipiantes/apps.js
+++ JsParaPrincipiantes/apps.js
@@ -1,1 +1,30 @@
 document.write("<h1>Hello world</h1>")
+
+"Hello World"//string
+'Hello world'//string
+
+1000000 //number
+-2.3
+
+//boolean
+true
+false
+
+//array
+['joe', 'ryan', martha]
+[1,2,3]
+[true, false, true, false]
+
+//object
+'ryan'
+70.4
+14
+true
+
+{
+    "username": 'ryan',
+    "score": 70.4
+    "hours": 14,
+    "proffesional":true
+}
+

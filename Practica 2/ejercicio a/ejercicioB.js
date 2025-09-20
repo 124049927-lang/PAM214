b782c6e4ab1330a5efd78313aee3653481e57538
124049927-lang <124049927@upq.edu.mx>
2025-09-19 19:39:02 -0600

    ejercicio b con filtrado de precios y el uso de map para la creación del nuevo arreglo

---
+++ Practica 2/ejercicio a/ejercicioB.js
@@ -0,0 +1,14 @@
+
+const productos = [
+  { nombre: "Laptop", precio: 12000 },
+  { nombre: "Mouse", precio: 250 },
+  { nombre: "Teclado", precio: 750 },
+  { nombre: "Monitor", precio: 3000 }
+];
+
+
+let filtrados = productos.filter(producto => producto.precio > 1000);
+
+let nombres = filtrados.map(producto => producto.nombre);
+
+console.log(nombres);

fa9f33d2c92b5a9875783631b3a1fed463b3773c
124049927-lang <124049927@upq.edu.mx>
2025-11-07 15:31:22 -0600

    2do commit examen

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,4 +1,4 @@
-import { ImageBackground, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
+import { ImageBackground, TextInput, Button, Alert, TouchableOpacity, showSplash } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-web';
@@ -13,33 +13,42 @@ export default function App() {
     return () => clearTimeout(timer);
   }, []);
 
-  if (showSplash) {
+    if (showSplash) {
     return (
       <View style = {styles.splashContainer}>
         <Text styles={styles.splashText>N}>Bienvenido a la APP </Text>
       </View>
-    );
+    )
   }
+
 
   return (
     <SafeAreaView style={{flex: 1}}>
       <ImageBackground 
-        source={{
-          uir:
-        }}
+        source={require('.assets/imagenes.jpeg')}
         style={styles.background}
         >
-          <View styles={styles.overlay}>
-            <Text style={styles.title}>Bienvenido!</Text>
+          <View style={styles.overlay}>
+            <Text style={styles.title}>Perfil</Text>
+
+            <Text style={styles.label}>Nombre: Rogelio Zea</Text> 
+            <Text style={styles.label}>Profeción: Estuduante</Text>
+            <Text style={styles.label}>Nacido16 de junio del año 2006 en la ciudad de Quéretaro.
+              Actualmente vive con sus padres y hermano en Querétaro. Estudia en la Universidad Politecnica de Querétaro 
+              y cursa la carrera de Tecnologias de la información he inovación digital. </Text>
+            <Text styles={styles.label}>corrreo:124049927@upq.edu.mx</Text>
+            <Text styles={styles.label}>Teléfono: 4421234567</Text>
           </View>
         </ImageBackground>
     </SafeAreaView>
   )
+}
+
 
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#323030ff',
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -63,6 +72,10 @@ export default function App() {
     },
 
     overlay:{
+      backgroundColor: 'rgba(0,0,0,0.5)',
+      padding: 20,
+      borderRadius:30,
+      width: '90%',
 
     }
 

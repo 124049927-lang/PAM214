e68e3ba1c9c2b122e1bc2d170a8b3083fc2be58d
124049927-lang <124049927@upq.edu.mx>
2025-11-07 14:50:29 -0600

    Primer commit de Examen

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
+import { ImageBackground, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-web';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [showSplash, setShowSplash] = useState(true);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return (
+      <View style = {styles.splashContainer}>
+        <Text styles={styles.splashText>N}>Bienvenido a la APP </Text>
+      </View>
+    );
+  }
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <ImageBackground 
+        source={{
+          uir:
+        }}
+        style={styles.background}
+        >
+          <View styles={styles.overlay}>
+            <Text style={styles.title}>Bienvenido!</Text>
+          </View>
+        </ImageBackground>
+    </SafeAreaView>
+  )
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    splashContainer: {
+      flex: 1,
+      backgroundColor: '#6a726aff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    splashText: {
+      fontSize: 32,
+      color: '#fff',
+    },
+
+    background:{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    overlay:{
+
+    }
+
+  });
